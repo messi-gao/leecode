385d0936b93440f54fe2478aede95870d073ddcd
gaoyh <gaoyihe@rongcapital.cn>
2020-01-29 11:41:53 +0800

    comment

--- src/dy/ClimbStairs.java
+++ src/dy/ClimbStairs.java
@@ -8,13 +8,13 @@ public class ClimbStairs {
         if (n == 0 || n == 1 || n == 2) {
             return n;
         }
-        int[] stairs = new int[n];
-        stairs[0] = 1;
-        stairs[1] = 2;
-        for (int i = 2; i < n; i++) {
+        int[] stairs = new int[n + 1];
+        stairs[1] = 1;
+        stairs[2] = 2;
+        for (int i = 3; i <= n; i++) {
             stairs[i] = stairs[i - 1] + stairs[i - 2];
         }
-        return stairs[n - 1];
+        return stairs[n];
     }
 
     public static void main(String[] args) {
--- src/dy/MinimumTotal.java
+++ src/dy/MinimumTotal.java
@@ -5,13 +5,36 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 120. 三角形最小路径和
+ * <p>
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ * <p>
+ * 例如，给定三角形：
+ * <p>
+ * [
+ * [2],
+ * [3,4],
+ * [6,5,7],
+ * [4,1,8,3]
+ * ]
+ * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+ * <p>
+ * 说明：
+ * <p>
+ * 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。
+ * <p>
+ *
+ * @author gaoyh
+ */
 public class MinimumTotal {
     public int minimumTotal(List<List<Integer>> triangle) {
         int row = triangle.size();
-        int[] minlen = new int[row+1];
-        for (int level = row-1;level>=0;level--){
-            for (int i = 0;i<=level;i++){   //第i行有i+1个数字
-                minlen[i] = Math.min(minlen[i], minlen[i+1]) + triangle.get(level).get(i);
+        int[] minlen = new int[row + 1];
+        for (int i = row - 1; i >= 0; i--) {
+            //第i行有i+1个数字
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                minlen[j] = Math.min(minlen[j], minlen[j + 1]) + triangle.get(i).get(j);
             }
         }
         return minlen[0];
@@ -20,9 +43,9 @@ public class MinimumTotal {
     public static void main(String[] args) {
         List<List<Integer>> list = new ArrayList<>();
         list.add(Collections.singletonList(1));
-        list.add(Arrays.asList(3,4));
-        list.add(Arrays.asList(6,5,7));
-        list.add(Arrays.asList(4,1,8,3));
+        list.add(Arrays.asList(3, 4));
+        list.add(Arrays.asList(6, 5, 7));
+        list.add(Arrays.asList(4, 1, 8, 3));
         System.out.println(new MinimumTotal().minimumTotal(list));
     }
 }
