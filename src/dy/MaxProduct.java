bf5c58a20d7b3362bd0abf647e577da1c038634f
gaoyh <gaoyihe@rongcapital.cn>
2020-02-17 23:06:03 +0800

    AddStrings

--- src/dy/MaxProduct.java
+++ src/dy/MaxProduct.java
@@ -1,23 +1,39 @@
 package dy;
 
+/**
+ * 152. 乘积最大子序列
+ * 给定一个整数数组 nums ，找出一个序列中乘积最大的连续子序列（该序列至少包含一个数）。
+ *
+ * 示例 1:
+ *
+ * 输入: [2,3,-2,4]
+ * 输出: 6
+ * 解释: 子数组 [2,3] 有最大乘积 6。
+ * 示例 2:
+ *
+ * 输入: [-2,0,-1]
+ * 输出: 0
+ * 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ * @author gaoyh
+ */
 public class MaxProduct {
     public int maxProduct(int[] nums) {
         if (nums == null) {
             return 0;
         }
-        int res = nums[0], max = nums[0], min = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            max = max * nums[i];
-            min = min * nums[i];
-
-            max = Math.max(Math.max(max, min), res);
-            min = Math.min(Math.min(max, min), res);
-
-            if (max > res) {
-                res = max;
+        int max = Integer.MIN_VALUE, imax = nums[0], imin = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] < 0){
+                int tmp = imax;
+                imax = imin;
+                imin = tmp;
             }
+            imax = Math.max(imax*nums[i], nums[i]);
+            imin = Math.min(imin*nums[i], nums[i]);
+
+            max = Math.max(max, imax);
         }
-        return res;
+        return max;
     }
 
     public static void main(String[] args) {
