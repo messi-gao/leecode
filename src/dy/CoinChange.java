032243737b216bdd7e6af547dceb9b8f03f1033d
gaoyh <gaoyihe@rongcapital.cn>
2020-03-21 16:07:31 +0800

    CoinChange.java

--- src/dy/CoinChange.java
+++ src/dy/CoinChange.java
@@ -31,18 +31,16 @@ public class CoinChange {
 
     public int coinChange(int[] coins, int amount) {
         int[] f = new int[amount + 1];
+        Arrays.fill(f, amount + 1);
         f[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            int cost = Integer.MAX_VALUE;
             for (int j = 0; j < coins.length; j++) {
                 if (i - coins[j] >= 0) {
-                    if (f[i - coins[j]] != Integer.MAX_VALUE)
-                        cost = Math.min(cost, f[i - coins[j]] + 1);
+                    f[i] = Math.min(f[i], f[i - coins[j]] + 1);
                 }
             }
-            f[i] = cost;
         }
-        return f[amount] == Integer.MAX_VALUE ? -1 : f[amount];
+        return f[amount] > amount ? -1 : f[amount];
     }
 
 
